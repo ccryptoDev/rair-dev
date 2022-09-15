691eafe30fe2acf90a4eb26a6cda5d54ae66c62b
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2022-09-15 06:13:02 -0400

    update NFT locker controller and user services

--- rairnode/bin/routes/v2.router.js
+++ rairnode/bin/routes/v2.router.js
@@ -4,8 +4,10 @@ const contractsController = require('../contracts/contracts.Controller');
 const uploadController = require('../media/controllers/index');
 const productsController = require('../products/product.Controller');
 const tokensController = require('../tokens/tokens.Controller');
+const locksController = require('../locks/locks.Controller');
 const resalesController = require('../resales/resales.Controller');
 const offersController = require('../offers/offers.Controller');
+const usersController = require('../users/users.Controller');
 const verifyController = require('../verification/userVerification.controller');
 const favoritesController = require('../favorites/favorites.Controller');
 const { JWTVerification } = require('../middleware');
@@ -16,10 +18,11 @@ module.exports = () => {
   router.use('/contracts', contractsController);
   router.use('/products', productsController);
   router.use('/tokens', tokensController());
+  router.use('/locks', locksController());
   router.use('/favorites', JWTVerification, favoritesController());
   router.use('/verify', verifyController());
   router.use('/upload', uploadController());
-
+  router.use('/users', usersController);
   router.use('/offers', offersController);
   router.use('/resales', resalesController);
   return router;
